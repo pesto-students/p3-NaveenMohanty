f699940d96fdd6ba561b93e833725a012238c177
Naveen Mohanty <softwareengineer12@bidonhomes.com>
2022-08-01 23:29:40 +0530

    problem 6.6 done

---
+++ Week-6/problem_6.6.js
@@ -0,0 +1,21 @@
+function threeSum(arr, num) {
+  let nearSum = Number.POSITIVE_INFINITY
+  let diffTillNow = Number.POSITIVE_INFINITY
+  let len = arr.length - 1
+  for (let i = 0; i <= (len - 2); i++) {
+    for (let j = 1; j <= (len - 1); j++) {
+      for (let k = 2; k <= len; k++) {
+        let sum = arr[i] + arr[j] + arr[k]
+        let diff = ((sum - num) < 0) ? (sum - num) * -1 : (sum - num)
+        if ((sum !== num) && (diffTillNow > diff)) {
+          nearSum = sum
+          diffTillNow = diff
+        }
+      }
+    }
+  }
+
+  return nearSum
+}
+
+console.log(threeSum([-1, 1, 1, -4], 1));
